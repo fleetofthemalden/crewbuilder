413ca3e5f9845179d943ef6eda6532c0bc6180a8
Dixon Minnick <public@minnick.co>
2023-03-24 17:28:24 +0000

    Eslint, App bar, colors (#3)

--- src/theme.ts
+++ src/theme.ts
@@ -5,10 +5,10 @@ import { red } from '@mui/material/colors';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#162B47',
     },
     secondary: {
-      main: '#19857b',
+      main: '#E7203B',
     },
     error: {
       main: red.A400,
